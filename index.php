f59082c5bd2f2687cc1359f3289f6bed0f73a2df
Girish Gupta <girish@girishgupta.com>
2021-11-01 14:31:33 -0700

    Gets rid of URL check

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 
-if ($_SERVER['SERVER_PORT']!=8888) // If I were on 8888, I'd be testing.
-{
-  if (substr($_SERVER['HTTP_HOST'],0,3)!="www")
-  {
-    header("Location: https://www.".$_SERVER['HTTP_HOST'].$_SERVER[REQUEST_URI]);
-  }
-}
-else
-{
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
+// if ($_SERVER['SERVER_PORT']!=8888) // If I were on 8888, I'd be testing.
+// {
+//   if (substr($_SERVER['HTTP_HOST'],0,3)!="www")
+//   {
+//     header("Location: https://www.".$_SERVER['HTTP_HOST'].$_SERVER[REQUEST_URI]);
+//   }
+// }
+// else
+// {
+//   ini_set('display_errors', 1);
+//   ini_set('display_startup_errors', 1);
+//   error_reporting(E_ALL);
 
-  #putenv("GOOGLE_APPLICATION_CREDENTIALS=girishgupta-f33157326a57.json"); // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-usage-php
-}
+//   #putenv("GOOGLE_APPLICATION_CREDENTIALS=girishgupta-f33157326a57.json"); // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-usage-php
+// }
 
 switch (@parse_url($_SERVER['REQUEST_URI'])['path'])
 {
